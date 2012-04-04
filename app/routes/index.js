58d5a735363c54432e5d954dd19756678d9de875
Matthew Solle <denopi@gmail.com>
2012-04-04 10:10:12 -0400

    change repo to fix upload

---
+++ app/routes/index.js
@@ -0,0 +1,30 @@
+
+
+/*
+ * GET home page.
+ */
+ 
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.awesome = function(req, res){
+  res.render('awesome.ejs', { title: 'awesome' })
+};
+
+exports.cool = function(req, res){
+  res.render('cool.ejs', { title: 'cool' })
+};
+
+exports.rad = function(req, res){
+  res.render('rad.ejs', { title: 'rad' })
+};
+
+exports.gnarly = function(req, res){
+  res.render('gnarly.ejs', { title: 'gnarly' })
+};
+
+exports.groovy = function(req, res){
+  res.render('groovy.ejs', { title: 'groovy' })
+};
